65772e83e3102c973874ceadb7588d67909e6b1e
newfund <newfund615@163.com>
2020-01-20 20:46:17 +0800

    First commit

---
+++ NEP5/NEP5/NEP5.cs
@@ -0,0 +1,128 @@
+﻿using Quras.SmartContract.Framework;
+using Quras.SmartContract.Framework.Services.Module;
+using Quras.SmartContract.Framework.Services.System;
+using System;
+using System.ComponentModel;
+using System.Numerics;
+using System.Text;
+
+namespace NEP5
+{
+    public class NEP5 : SmartContract
+    {
+        [DisplayName("transfer")]
+        public static event Action<byte[], byte[], BigInteger> Transferred;
+
+        private static readonly byte[] Owner = "DpRpggGCuP1qd7KSKMA7wqPMMkM2z8rQe3".ToScriptHash(); //Owner Address
+        private static readonly BigInteger TotalSupplyValue = 10000000000000000;
+
+        public static object Main(string method, object[] args)
+        {
+            if (Runtime.Trigger == TriggerType.Verification)
+            {
+                return Runtime.CheckWitness(Owner);
+            }
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                var callscript = ExecutionEngine.CallingScriptHash;
+                
+                if (method == "balanceOf") return BalanceOf((byte[])args[0]);
+
+                if (method == "decimals") return Decimals();
+
+                if (method == "name") return Name();
+
+                if (method == "symbol") return Symbol();
+
+                if (method == "supportedStandards") return SupportedStandards();
+
+                if (method == "totalSupply") return TotalSupply();
+
+                if (method == "deploy") return Deploy();
+
+                if (method == "transfer") return Transfer((byte[])args[0], (byte[])args[1], (BigInteger)args[2], callscript);
+            }
+            return false;
+        }
+
+        [DisplayName("balanceOf")]
+        public static BigInteger BalanceOf(byte[] account)
+        {
+            if (account.Length != 20)
+                throw new InvalidOperationException("The parameter account SHOULD be 20-byte addresses.");
+            StorageMap asset = Storage.CurrentContext.CreateMap(nameof(asset));
+            return asset.Get(account).AsBigInteger();
+        }
+
+        [DisplayName("decimals")]
+        public static byte Decimals() => 8;
+
+        private static bool IsPayable(byte[] to)
+        {
+            var c = Blockchain.GetContract(to);
+            return c == null || c.IsPayable;
+        }
+
+        [DisplayName("totalSupply")]
+        public static BigInteger TotalSupply()
+        {
+            StorageMap contract = Storage.CurrentContext.CreateMap(nameof(contract));
+            return contract.Get("totalSupply").AsBigInteger();
+        }
+
+        [DisplayName("deploy")]
+        public static bool Deploy()
+        {
+            if (TotalSupply() != 0) return false;
+            StorageMap contract = Storage.CurrentContext.CreateMap(nameof(contract));
+            contract.Put("totalSupply", TotalSupplyValue);
+            StorageMap asset = Storage.CurrentContext.CreateMap(nameof(asset));
+            asset.Put(Owner, TotalSupplyValue);
+            Transferred(null, Owner, TotalSupplyValue);
+            return true;
+        }
+
+        [DisplayName("name")]
+        public static string Name() => "LorryToken"; //name of the token
+
+        [DisplayName("symbol")]
+        public static string Symbol() => "LOR"; //symbol of the token
+
+        [DisplayName("supportedStandards")]
+        public static string[] SupportedStandards() => new string[] { "NEP-5", "NEP-7", "NEP-10" };
+
+        [DisplayName("transfer")]
+        private static bool Transfer(byte[] from, byte[] to, BigInteger amount, byte[] callscript)
+        {
+            //Check parameters
+            if (from.Length != 20 || to.Length != 20)
+                throw new InvalidOperationException("The parameters from and to SHOULD be 20-byte addresses.");
+            if (amount <= 0)
+                throw new InvalidOperationException("The parameter amount MUST be greater than 0.");
+            // TODO: Uncomment
+            /*if (!IsPayable(to))
+                return false;*/
+            if (!Runtime.CheckWitness(from) && from.AsBigInteger() != callscript.AsBigInteger())
+                return false;
+            StorageMap asset = Storage.CurrentContext.CreateMap(nameof(asset));
+            var fromAmount = asset.Get(from).AsBigInteger();
+            if (fromAmount < amount)
+                return false;
+            if (from == to)
+                return true;
+
+            //Reduce payer balances
+            if (fromAmount == amount)
+                asset.Delete(from);
+            else
+                asset.Put(from, fromAmount - amount);
+            
+            //Increase the payee balance
+            var toAmount = asset.Get(to).AsBigInteger();
+            asset.Put(to, toAmount + amount);
+            
+            Transferred(from, to, amount);
+            return true;
+        }
+    }
+}
---
+++ Tutorial/HelloWorld/Contract1.cs
@@ -0,0 +1,19 @@
+﻿using Quras.SmartContract.Framework;
+using Quras.SmartContract.Framework.Services.Module;
+using System;
+using System.Numerics;
+
+namespace HelloWorld
+{
+    public class Contract1 : SmartContract
+    {
+        private static readonly string version = "1.0.0";
+
+        public static bool Main(string operation, object[] args)
+        {
+            Runtime.Log(version);
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+            return true;
+        }
+    }
+}
